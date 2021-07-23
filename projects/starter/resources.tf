67e4ea2713afa69d1c0ae183996cf819786cecc3
bachmeb <bachmeb@mail.uc.edu>
2021-07-22 22:04:48 -0400

    add providers and resources

---
+++ projects/starter/resources.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-asdfasdfasdfasdfasdf"
+  acl    = "private"
+  force_destroy = true
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
