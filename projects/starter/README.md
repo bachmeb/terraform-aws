976a0d6d217671c27a015ab4f9cd8e0dd9c38722
bachmeb <bachmeb@mail.uc.edu>
2021-07-25 17:04:45 -0400

    terraform fmt

---
+++ projects/starter/README.md
@@ -0,0 +1,14 @@
+# aws resources
+
+
+
+* sns
+* api-gateway
+* lambda
+* vpc
+* subnet
+* security group
+* iam
+* step function
+* ec2
+
--- projects/starter/resources.tf
+++ projects/starter/resources.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-asdfasdfasdfasdfasdf"
-  acl    = "private"
+  bucket        = "my-tf-test-bucket-asdfasdfasdfasdfasdf"
+  acl           = "private"
   force_destroy = true
   tags = {
     Name        = "My bucket"
